0731b008fd81861b02f58dc8cac6c2a7f647b19f
pubuzhixing8 <pubuzhixing@gmail.com>
2021-05-08 16:28:55 +0800

    fix(view): remove repeat input

--- packages/src/components/leaf/default-leaf.component.ts
+++ packages/src/components/leaf/default-leaf.component.ts
@@ -4,7 +4,7 @@ import { BaseLeafComponent } from "../../view/base";
 
 @Component({
     selector: 'span[slateDefaultLeaf]',
-    template: `<span slateString [context]="context" [viewContext]="viewContext" [viewContext]="viewContext"><span>`,
+    template: `<span slateString [context]="context" [viewContext]="viewContext"><span>`,
     changeDetection: ChangeDetectionStrategy.OnPush,
     host: {
         'data-slate-leaf': 'true'
