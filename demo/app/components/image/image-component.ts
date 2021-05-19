c89986ff04e43c261ea94e9af3bdce9dd3bad59d
王焕 <2323666315@qq.com>
2021-05-19 17:04:33 +0800

    feat(demo): add images demo #WIK-563

--- demo/app/components/image/image-component.ts
+++ demo/app/components/image/image-component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ChangeDetectorRef, HostListener, Output, EventEmitter } from '@angular/core';
 import { BaseElementComponent } from 'slate-angular';
 
 @Component({
-    selector: 'demo-element-image',
-    templateUrl: 'image-component.html'
+    selector: 'demo-element-image,[demoImage]',
+    template: `<img [src]="element.url" alt="" contenteditable="false">`,
+    host: {
+        '[attr.contenteditable]': 'false',
+        class: 'demo-element-image'
+    }
 })
 export class DemoElementImageComponent extends BaseElementComponent {
 }
