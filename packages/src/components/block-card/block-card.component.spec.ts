d03ad1ec792c291903f6a5cba9c8fc3364086e0a
pubuzhixing8 <pubuzhixing@gmail.com>
2021-06-16 11:43:42 +0800

    chore(view): add block card unit test

---
+++ packages/src/components/block-card/block-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, fakeAsync, flush, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ImageEditableComponent, configureBasicEditableTestingModule } from '../../testing';
+
+describe('Block Card Component', () => {
+    let component: ImageEditableComponent;
+    let fixture: ComponentFixture<ImageEditableComponent>;
+
+    beforeEach(fakeAsync(() => {
+        configureBasicEditableTestingModule([ImageEditableComponent]);
+        fixture = TestBed.createComponent(ImageEditableComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        flush();
+        fixture.detectChanges();
+    }));    
+
+    it('The block-card component should be created', fakeAsync(() => {
+        let blockCardElement: HTMLElement;
+        blockCardElement = fixture.debugElement.query(By.css('.slate-block-card')).nativeElement;
+        expect(blockCardElement).toBeTruthy();
+    }));
+});
--- packages/src/testing/index.ts
+++ packages/src/testing/index.ts
@@ -4,4 +4,5 @@ export * from './element-focus';
 export * from './module';
 export * from './basic-editable.component';
 export * from './advanced-editable.component';
+export * from './image-editable.component';
 export * from './leaf.component';
