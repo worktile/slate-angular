9b3db19b208c941f593bac31065ab08d4b7f058b
donaldxdonald <mzhlovefama@gmail.com>
2022-11-10 16:12:23 +0800

    fix(core): fix legacy check for edge and firefox

--- packages/src/utils/environment.ts
+++ packages/src/utils/environment.ts
@@ -21,7 +21,7 @@ export const IS_SAFARI =
 // "modern" Edge was released at 79.x
 export const IS_EDGE_LEGACY =
     typeof navigator !== 'undefined' &&
-    /Edge?\/(?:[0-6][0-9]|[0-7][0-8])/i.test(navigator.userAgent);
+    /Edge?\/(?:[0-6][0-9]|[0-7][0-8])(?:\.)/i.test(navigator.userAgent);
 
 export const IS_CHROME =
     typeof navigator !== 'undefined' && /Chrome/i.test(navigator.userAgent);
@@ -37,7 +37,7 @@ export const IS_CHROME_LEGACY =
 // Firefox did not support `beforeInput` until `v87`.
 export const IS_FIREFOX_LEGACY =
     typeof navigator !== 'undefined' &&
-    /^(?!.*Seamonkey)(?=.*Firefox\/(?:[0-7][0-9]|[0-8][0-6])).*/i.test(
+    /^(?!.*Seamonkey)(?=.*Firefox\/(?:[0-7][0-9]|[0-8][0-6])(?:\.)).*/i.test(
         navigator.userAgent
     );
 
