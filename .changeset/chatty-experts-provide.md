9301665a78ec101f38d9687871b10fc664a397b8
pubuzhixing8 <pubuzhixing@gmail.com>
2023-03-20 15:11:32 +0800

    feat(core): android input handing #216

---
+++ .changeset/chatty-experts-provide.md
@@ -0,0 +1,5 @@
+---
+"slate-angular": minor
+---
+
+android input handing
---
+++ packages/src/utils/restore-dom.ts
@@ -0,0 +1,36 @@
+import { Editor } from 'slate';
+import { EDITOR_TO_ELEMENT } from './weak-maps';
+
+export function restoreDom(editor: Editor, execute: () => void) {
+    const editable = EDITOR_TO_ELEMENT.get(editor);
+    let observer = new MutationObserver(mutations => {
+        mutations.reverse().forEach(mutation => {
+            if (mutation.type === 'characterData') {
+                // We don't want to restore the DOM for characterData mutations
+                // because this interrupts the composition.
+                return;
+            }
+
+            mutation.removedNodes.forEach(node => {
+                mutation.target.insertBefore(node, mutation.nextSibling);
+            });
+
+            mutation.addedNodes.forEach(node => {
+                mutation.target.removeChild(node);
+            });
+        });
+        disconnect();
+        execute();
+    });
+    const disconnect = () => {
+        observer.disconnect();
+        observer = null;
+    };
+    observer.observe(editable, { subtree: true, childList: true, characterData: true, characterDataOldValue: true });
+    setTimeout(() => {
+        if (observer) {
+            disconnect();
+            execute();
+        }
+    }, 0);
+}
