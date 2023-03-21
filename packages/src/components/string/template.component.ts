4452f922fb5f53dbacb4ddd10ac4e48af668e13e
pubuzhixing8 <pubuzhixing@gmail.com>
2023-03-21 09:40:59 +0800

    chore: remove useless code

--- packages/src/components/string/template.component.ts
+++ packages/src/components/string/template.component.ts
@@ -6,9 +6,6 @@ import { Component, ChangeDetectionStrategy, ViewChild, TemplateRef } from '@ang
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SlateStringTemplateComponent {
-    // @ViewChild('stringTemplate', { read: TemplateRef, static: true })
-    // stringTemplate: TemplateRef<any>;
-
     @ViewChild('compatStringTemplate', { read: TemplateRef, static: true })
     compatStringTemplate: TemplateRef<any>;
 
