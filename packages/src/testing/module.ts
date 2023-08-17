db9618b8d223371401cb2d8175bab06ce549bada
cmm-va <867884660@qq.com>
2023-08-17 15:08:37 +0800

    build: bump angular to v16

--- packages/src/testing/module.ts
+++ packages/src/testing/module.ts
@@ -1,11 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { NgModule, Provider } from '@angular/core';
+import { Provider } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
 import { SlateModule } from '../module';
 import { FormsModule } from '@angular/forms';
-import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
-import { TestingLeafComponent } from './leaf.component';
 
 export function configureBasicEditableTestingModule(declarations: any[], entryComponents: any[] = [], providers: Provider[] = []) {
     TestBed.configureTestingModule({
@@ -13,11 +11,5 @@ export function configureBasicEditableTestingModule(declarations: any[], entryCo
         declarations: [...declarations],
         providers: [...providers],
         teardown: { destroyAfterEach: false }
-    })
-        .overrideModule(BrowserDynamicTestingModule, {
-            set: {
-                entryComponents: entryComponents
-            }
-        })
-        .compileComponents();
+    }).compileComponents();
 }
