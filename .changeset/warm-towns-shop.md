55dbfedf0a3018fd89355095e22fed487905e037
xinglu01 <35676233+xinglu01@users.noreply.github.com>
2026-02-03 16:58:21 +0800

    build: bump angular 21 (#334)
    * build: bump angular 21
    
    * build: bump angular version to 21.1.2
    
    * chore: pre enter next
    
    ---------
    
    Co-authored-by: pubuzhixing8 <pubuzhixing@gmail.com>

---
+++ .changeset/warm-towns-shop.md
@@ -0,0 +1,5 @@
+---
+'slate-angular': major
+---
+
+bump angular version to 21.1.2
--- packages/src/testing/module.ts
+++ packages/src/testing/module.ts
@@ -1,15 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Provider } from '@angular/core';
+import { Provider, provideZoneChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { SlateModule } from '../module';
-import { FormsModule } from '@angular/forms';
 
 export function configureBasicEditableTestingModule(declarations: any[], entryComponents: any[] = [], providers: Provider[] = []) {
     TestBed.configureTestingModule({
         declarations: declarations,
         imports: [CommonModule, BrowserModule, SlateModule, FormsModule],
-        providers: [...providers],
+        providers: [...providers, provideZoneChangeDetection()],
         teardown: { destroyAfterEach: false }
     }).compileComponents();
 }
